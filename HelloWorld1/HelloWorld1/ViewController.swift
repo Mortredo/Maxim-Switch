1a60b43b344fba74c47d4591c71e0af40c6e7799
Mortredo <36920438+Mortredo@users.noreply.github.com>
2020-04-09 17:44:23 +0300

    Hello World 1

---
+++ HelloWorld1/HelloWorld1/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  HelloWorld1
+//
+//  Created by Максим on 05.03.2020.
+//  Copyright © 2020 Merber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+}
